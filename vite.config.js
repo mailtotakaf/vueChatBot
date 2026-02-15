05d6c21cd47b9c5053cb32a1d449ea0b47c5fe34
tak <mailtotakaf@gmail.com>
2026-02-15 16:04:41 +0900

    addPWA

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate', // ユーザーのアプリも自動更新
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'vueChatBot',
+        short_name: 'vueChatBot',
+        description: 'vueChatBot',
+        theme_color: '#42b983',
+        background_color: '#ffffff',
+        display: 'standalone', // これが「アプリっぽく」表示する鍵です
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
